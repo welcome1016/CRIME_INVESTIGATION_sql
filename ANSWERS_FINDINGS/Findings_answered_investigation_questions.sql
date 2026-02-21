700e646b8021be9ec422ec0f898253927877a145
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-02-21 23:35:15 +0200

    answered fe questions

--- ANSWERS_FINDINGS/Findings_answered_investigation_questions.sql
+++ ANSWERS_FINDINGS/Findings_answered_investigation_questions.sql
@@ -5,16 +5,87 @@
 	* FROM south_africa_criminal_info
 
 --2. Count the total number of cases in the table.
+							SELECT COUNT(*)
+							AS TOTAL_NUMBER_OF_CASES 
+							FROM south_africa_criminal_info
 
 --3. Count the number of cases in each province.
+	SELECT PROVINCE ,count(CaseNumber)
+	AS TOTAL_CASES_PER_PROVINCE 
+	FROM south_africa_criminal_info
+	GROUP BY PROVINCE
+	ORDER BY PROVINCE DESC;
+
+
 --4. Retrieve all cases where the crime type is Fraud.
+		SELECT CrimeType,RecordID,FirstName,LastName,Age,Gender,Country,Province
+		from south_africa_criminal_info
+		Where CrimeType ='Fraud';
+					
 --5. Count the number of cases for each crime type.
+						SELECT DISTINCT CrimeType 
+						from south_africa_criminal_info
+						
+						--here I found out which type of crimes are in the Table
+						SELECT CrimeType,COUNT(CrimeType)
+						As Number_of_cases_per_CrimeType
+						from south_africa_criminal_info
+						Group by CrimeType
+						
+
+
+		
 --6. Display all cases classified as High or Critical risk.
+	SELECT RISKLEVEL
+	FROM south_africa_criminal_info
+	WHERE RISKLEVEL ='CRITICAL'
+
+					--For high risk
+						SELECT RISKLEVEL
+							FROM south_africa_criminal_info
+							WHERE RISKLEVEL ='high'
+
+		
+
+	-----I'm trying to find a way of getting all it together with a single code
+				SELECT RISKLEVEL,RecordID,FirstName,LastName,FullName
+	FROM south_africa_criminal_info
+	WHERE RISKLEVEL IN ('High', 'Critical')
+	Order by RISKLEVEL DESC
+	
+		
+
 --7. Calculate the average Financial Score of all suspects.
+		SELECT Avg(financialScore)
+		as Average_FinacialScore
+		from south_africa_criminal_info
+		
 --8. Find the highest Estimated Fraud Amount recorded.
+	SELECT TOP 1 EstimatedFraudAmount_ZAR,RecordID,FirstName,LastName,FullName,RecordID,FirstName,LastName
+	AS HIGHEST_FRAUD_RECORDED
+	from south_africa_criminal_info
+	ORDER BY  EstimatedFraudAmount_ZAR DESC;
+
+	
+		
 --9. Retrieve the top 10 cases with the highest Estimated Fraud Amount.
+
+	SELECT TOP 10 EstimatedFraudAmount_ZAR,RecordID,FirstName,LastName,FullName
+	AS TOP_10_HIGHEST_FRAUD_RECORDED
+	from south_africa_criminal_info
+	ORDER BY  EstimatedFraudAmount_ZAR DESC;
+
 --10. Display all cases where the suspect was arrested.
+	SELECT Arrested,RecordID,FirstName,LastName,FullName
+	AS SUSPECTS_ARRESTED
+	from south_africa_criminal_info
+	WHERE Arrested ='Yes'
 --11. Count how many suspects were arrested versus not arrested.
+SELECT Arrested,COUNT(*)
+AS NUMBER_OF_SUSPECTS_ARRESTED_VS_NOT_ARRESTED
+from south_africa_criminal_info
+Group by Arrested
+
 --12. Count the number of cases associated with each bank.
 --13. Calculate the total Estimated Fraud Amount per province.
 --14. Retrieve all suspects older than 50 years.
